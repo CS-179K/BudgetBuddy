67d8c1cb20bcb688cd62ad82c3e89a5d6c95e313
aguin005 <aguin005@ucr.edu>
2024-08-25 12:30:44 -0700

    - Create toggle for months

--- frontend/src/components/SpendingSummary.js
+++ frontend/src/components/SpendingSummary.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useInvestmentsContext } from '../hooks/useInvestmentsContext';
 import { useIncomesContext } from '../hooks/useIncomesContext';
 import CategoryBarChart from './CategoryBarChart';
+import './InvestmentForm.css';
 
 const SpendingSummary = () => {
     const { investments } = useInvestmentsContext();
@@ -15,6 +16,24 @@ const SpendingSummary = () => {
     return (
         <div className="spending-summary">
             <p>Total monthly investments: ${totalInvestmentValue.toFixed(2)}</p>
+            <form className="create">
+                <label>Month:</label>
+                <select>
+                    <option value=""></option>
+                    <option value="january">January</option>
+                    <option value="february">February</option>
+                    <option value="march">March</option>
+                    <option value="april">April</option>
+                    <option value="may">May</option>
+                    <option value="june">June</option>
+                    <option value="july">July</option>
+                    <option value="august">August</option>
+                    <option value="september">September</option>
+                    <option value="october">October</option>
+                    <option value="november">November</option>
+                    <option value="december">December</option>
+                </select>
+            </form>
             <CategoryBarChart/>
             <p>Percentage of income spent: {investmentPercentage.toFixed(2)}%</p>
             <p>Remaining income after investments: ${remainingIncome.toFixed(2)}</p>
