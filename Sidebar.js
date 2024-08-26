57a60993dfe0161b9c2c079f485bdbe33e9b8c80
Bhashyam <88957580+BhashyamS@users.noreply.github.com>
2024-08-26 04:40:29 -0700

    Add files via upload

--- Sidebar.js
+++ Sidebar.js
@@ -1,8 +1,10 @@
-// Sidebar.js
 import React from 'react';
 
 const Sidebar = ({ setActiveView, notifications }) => {
-  const hasNotifications = notifications.length > 0; // Check if there are any notifications
+  // Ensure notifications is always an array before accessing its properties
+  const safeNotifications = notifications || [];
+  const hasNotifications = safeNotifications.length > 0; // Check if there are any notifications
+  
   const notificationButtonStyle = {
     backgroundColor: hasNotifications ? 'red' : 'inherit', // Change background color if there are notifications
     color: hasNotifications ? 'white' : 'inherit', // Change text color for better visibility on red background
@@ -14,13 +16,14 @@ const Sidebar = ({ setActiveView, notifications }) => {
         <li className="sidebar"><button onClick={() => setActiveView('investments')}>Investments</button></li>
         <li className="sidebar"><button onClick={() => setActiveView('budgets')}>Budgets</button></li>
         <li className="sidebar"><button onClick={() => setActiveView('incomes')}>Incomes</button></li>
-	<li className = "sidebar"><button onClick={() => setActiveView('statements')}>Statements</button></li>
+        <li className="sidebar"><button onClick={() => setActiveView('statements')}>Statements</button></li>
+        <li className = "sidebar"><button onClick={() => setActiveView('spendingSummary')}>Spending Summary</button></li>
         <li className="sidebar">
           <button 
             style={notificationButtonStyle} 
             onClick={() => setActiveView('Notifications')}
           >
-            Notifications {hasNotifications && <span>({notifications.length})</span>}
+            Notifications {hasNotifications && <span>({safeNotifications.length})</span>}
           </button>
         </li>
       </ul>
